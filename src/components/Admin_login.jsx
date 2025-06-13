2380a6807c0549ba48cf4db0fd5f2d5d1f898b66
RaghavBhargava <raghavbhargava3@gmail.com>
2025-06-13 20:41:59 +0530

    deployment-adjustments

--- src/components/Admin_login.jsx
+++ src/components/Admin_login.jsx
@@ -29,6 +29,9 @@ const AdminLogin = () => {
       if (data.isAdmin) {
         localStorage.setItem('isAdmin', 'true');
         localStorage.setItem('admin_name', adminCredentials.username);
+        console.log("Saved isAdmin =", localStorage.getItem('isAdmin'));
+        console.log("Saved admin_name =", localStorage.getItem('admin_name'));
+
         setTimeout(() => {
           navigate('/admin-home');
         }, 100); // 100ms delay gives React time to catch up
